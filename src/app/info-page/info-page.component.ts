081caf3912b4a6123cca4ecb3b5326f5555e5ce9
rozhko.dmytro <dimarozhko2000@gmail.com>
2023-03-09 01:37:47 +0200

    LAB-1 info page

--- src/app/info-page/info-page.component.ts
+++ src/app/info-page/info-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InfoGame } from '../model/info-game';
 
 @Component({
   selector: 'app-info-page',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./info-page.component.css']
 })
 export class InfoPageComponent {
+  infoGames: InfoGame[];
 
+  constructor() {
+    this.infoGames = [
+      {
+        name: '15',
+        rule: 'do something do something do something do something do something do something do something do something',
+        history: 'interesting story interesting story interesting story interesting story interesting story interesting story'
+      }
+    ]
+  }
 }
---
+++ src/app/model/info-game.ts
@@ -0,0 +1,5 @@
+export interface InfoGame {
+    name: string;
+    rule: string;
+    history: string;
+}
