ec0e0b946332f94d890e9cd6d6021b85f05579c2
rozhko.dmytro <dimarozhko2000@gmail.com>
2023-03-09 02:54:17 +0200

    LAB-1 init game page

--- src/app/game-page/game-page.component.ts
+++ src/app/game-page/game-page.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class GamePageComponent {
 
+  gameField: string[][];
+
+  constructor() {
+    this.gameField = [
+      ['1', '2', '3', '4'],
+      ['5', '6', '7', '8'],
+      ['9', '10', '11', '12'],
+      ['13', '14', '15', '-']
+    ]
+  }
 }
