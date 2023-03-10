4bc88a777a3a6347ab030e586d4348b1ed822e45
rozhko.dmytro <dimarozhko2000@gmail.com>
2023-03-10 02:47:54 +0200

    LAB-1 game concept done

--- src/app/game-page/game-page.component.ts
+++ src/app/game-page/game-page.component.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
 export class GamePageComponent {
 
   gameField: string[][];
+  isClick: boolean;
+  savedEventList: any[];
 
   constructor() {
     this.gameField = [
@@ -15,6 +17,36 @@ export class GamePageComponent {
       ['5', '6', '7', '8'],
       ['9', '10', '11', '12'],
       ['13', '14', '15', '-']
-    ]
+    ];
+    this.isClick = false;
+    this.savedEventList = [];
+  }
+
+  onMouseOverItem(event: any) : void {
+    event.srcElement.classList.add('sgp-bth-background-press');
+    this.savedEventList.push(event);
+  }
+
+  onMouseOutItem(event: any): void {
+    if (this.isClick === false) {
+      event.srcElement.classList.remove('sgp-bth-background-press');
+    }
+  }
+
+  onMouseDown(event: any): void {
+    this.isClick = true;
+    event.srcElement.classList.add('sgp-bth-background-press');
+    this.savedEventList.push(event);
+  }
+
+  onMouseUp(event: any): void {
+    this.isClick = false;
+    this.savedEventList.forEach(savedEvent => this.onMouseOutItem(savedEvent));
+    console.log(this.savedEventList);
+    if (this.savedEventList.length === 2) {
+      console.log("MOVE");
+      // this.savedEventList.forEach(savedEvent => console.log(savedEvent));
+    }
+    this.savedEventList = [];
   }
 }
