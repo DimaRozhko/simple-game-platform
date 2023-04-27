8e84730c442129f3f8faaa2f764ea0271277d65c
rozhko.dmytro <dimarozhko2000@gmail.com>
2023-04-28 01:30:08 +0300

    LAB-2 cyrpess test done

---
+++ LAB-2/simple-game-platform/cypress/e2e/sidebar-navigation-scenario.spec.cy.ts
@@ -0,0 +1,50 @@
+describe('[simple-game-platform] End-2-End test. Sidebar navigation scenario', () => {
+
+  it('Visits user-page. Navigate to information, game pages', () => {
+    cy.visit('/user-page');
+    cy.get('a[href="/info-page"]').invoke('attr', 'href')
+      .then(href => {
+        cy.visit(href ? href : '/user-page');
+      });
+    cy.url().should('include', '/info-page');
+
+    cy.visit('/user-page');
+    cy.get('a[href="/game-page"]').invoke('attr', 'href')
+      .then(href => {
+        cy.visit(href ? href : '/user-page');
+      });
+    cy.url().should('include', '/game-page');
+  });
+
+  it('Visits info-page. Navigate to user, game pages', () => { 
+    cy.visit('/info-page');
+    cy.get('a[href="/user-page"]').invoke('attr', 'href')
+      .then(href => {
+        cy.visit(href ? href : '/info-page');
+      });
+    cy.url().should('include', '/user-page');
+
+    cy.visit('/info-page');
+    cy.get('a[href="/game-page"]').invoke('attr', 'href')
+      .then(href => {
+        cy.visit(href ? href : '/info-page');
+      });
+    cy.url().should('include', '/game-page');
+  });
+
+  it('Visits game-page. Navigate to user, information pages', () => { 
+    cy.visit('/game-page');
+    cy.get('a[href="/user-page"]').invoke('attr', 'href')
+      .then(href => {
+        cy.visit(href ? href : '/game-page');
+      });
+    cy.url().should('include', '/user-page');
+
+    cy.visit('/game-page');
+    cy.get('a[href="/info-page"]').invoke('attr', 'href')
+      .then(href => {
+        cy.visit(href ? href : '/game-page');
+      });
+    cy.url().should('include', '/info-page');
+  });
+})
---
+++ LAB-2/simple-game-platform/cypress/e2e/sign-up-scenario.spec.cy.ts
@@ -0,0 +1,33 @@
+describe('[simple-game-platform] End-2-End test. Sign up scenario', () => {
+
+
+  it('Visits sign-up page -> input data -> log in', () => {
+    cy.visit('/sign-up');
+    cy.get("#sgp-authorisation-field").should('exist');
+    cy.get('input[placeholder=username]').type('CypressUser', { force: true });
+    cy.get('input[placeholder=email]').type('hello@cypress.io', { force: true });
+    cy.get('input[placeholder=password]').type('Cypress1', { force: true });
+    cy.get('a[href="/user-page"]').invoke('attr', 'href')
+      .then(href => {
+        cy.visit(href ? href : '/sign-up');
+      });
+    cy.url().should('include', '/user-page');
+  })
+
+  it('Visits log-in page -> navigate to sign-up -> input data -> log in', () => {
+    cy.visit('/log-in');
+    cy.get('a[href="/sign-up"]').invoke('attr', 'href')
+      .then(href => {
+        cy.visit(href ? href : '/log-in');
+      });
+    cy.get("#sgp-authorisation-field").should('exist');
+    cy.get('input[placeholder=username]').type('CypressUser', { force: true });
+    cy.get('input[placeholder=email]').type('hello@cypress.io', { force: true });
+    cy.get('input[placeholder=password]').type('Cypress1', { force: true });
+    cy.get('a').invoke('attr', 'href')
+      .then(href => {
+        cy.visit(href ? href : '/sign-up');
+      });
+    cy.url().should('include', '/user-page');
+  })
+})
